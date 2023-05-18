cef853bebc0f5f82c8678a3d12da80c30ecd5adc
Kirpal Singh <122300834+kirpalsingh225@users.noreply.github.com>
2023-05-18 21:10:25 +0530

    Add files via upload

---
+++ codes/day48.java
@@ -0,0 +1,12 @@
+public class day48{
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root!=null){
+            bstToGst(root.right);
+            sum+=root.val;
+            root.val=sum;
+            bstToGst(root.left);
+        }
+        return root;
+    } 
+}
